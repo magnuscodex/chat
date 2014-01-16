c5350913e682a9c9fcde2f7ccc9290f0de077101
Mike Roberts <magnuscodex@gmail.com>
2014-01-16 11:42:51 -0800

    Client should be able to send and receive multiple times. Also, some work that should make client handle incoming messages from other clients better.

--- client.py
+++ client.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 
 import socket
+import sys
+from select import select
 
-TCP_IP = "127.0.0.1"
 TCP_PORT = 4675
 BUFF_SIZE = 1024
-MESSAGE = "THIS IS A TEST! PLEASE IGNORE!"
+TIMEOUT = 1
+
+host = raw_input("server name:")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFF_SIZE)
+s.connect((host, TCP_PORT))
+while True:
+  rlist, _, _ = select([sys.stdin], [], [], TIMEOUT)
+  if rlist:
+    msg = sys.stdin.readline()
+    s.send(msg)
+  received = True
+  while received:
+    rlist, _, _ = select([s], [], [], TIMEOUT)
+    if rlist:
+      data = s.recv(BUFF_SIZE)
+      print data,
+    else:
+      received = False
+      
 s.close()
 
-print "received data:", data
 
